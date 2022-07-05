7339c6733816ea7f25cb9e860cc9f43ce0701d50
Evghenii R <108763119+Djeket78@users.noreply.github.com>
2022-07-06 00:41:34 +0300

    Add files via upload

---
+++ 4.07 risunok.py
@@ -0,0 +1,20 @@
+figure=input("what figue to draw?")
+
+if figure == "line":
+    print("-----")
+elif figure == "square":
+    print("-----")
+    print("|   |")
+    print("|   |")
+    print("-----")
+elif figure == "phl":  #parallel horizontal lines
+    print("-----")
+    print("-----")
+elif figure =="pvl":   #parallel vertical lines
+    print("|   |")
+    print("|   |")
+    
+else:
+    print("CAN'T DRAW SUCH FIGURE!" )
+
+ # optimizirovati ???
